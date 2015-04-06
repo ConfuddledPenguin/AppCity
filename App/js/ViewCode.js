f3a1671262b17d892e65072d1ba442eebc7053d0
Thomas Maxwell <hello@tom-maxwell.com>
2015-04-06 10:53:44 +0100

    Tidying up the front end frame a little

--- App/js/ViewCode.js
+++ App/js/ViewCode.js
@@ -1,12 +1,57 @@
 var menuVisible = false;
+var currentView = $("#explore");
 
-$("#menu-icon").click(function(event) {
-	console.log($('#menu'));
+function menutoggle(){
 	if(!menuVisible){
 		$("#menu").addClass('menu-visible');
+		$("main").addClass('fade-out');
 		menuVisible = true;
 	}else{
 		$("#menu").removeClass('menu-visible');
+		$("main").removeClass('fade-out');
 		menuVisible = false;
 	}
+}
+
+$("#menu-icon").click(menutoggle);
+
+$("#menu-Explore").click(function(event) {
+	currentView.addClass('noDisplay');
+	$("#explore").removeClass('noDisplay');
+	currentView = $("#explore");
+	$("#headerTitle").text("Explore");
+	menutoggle();
 });
+
+$("#menu-Guides").click(function(event) {
+	currentView.addClass('noDisplay');
+	$("#guides").removeClass('noDisplay');
+	currentView = $("#guides");
+	$("#headerTitle").text("Guides");
+	menutoggle();
+});
+
+$("#menu-User").click(function(event) {
+	currentView.addClass('noDisplay');
+	$("#user").removeClass('noDisplay');
+	currentView = $("#user");
+	$("#headerTitle").text("#UserName");
+	menutoggle();
+});
+
+$("#menu-Settings").click(function(event) {
+	currentView.addClass('noDisplay');
+	$("#settings").removeClass('noDisplay');
+	currentView = $("#settings");
+	$("#headerTitle").text("Settings");
+	menutoggle();
+});
+
+$("#menu-About").click(function(event) {
+	currentView.addClass('noDisplay');
+	$("#about").removeClass('noDisplay');
+	currentView = $("#about");
+	$("#headerTitle").text("About");
+	menutoggle();
+});
+
